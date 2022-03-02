ae114ba341d857552efd0f48e8b87cb4124b2a06
kalya <kalya@LAPTOP-811GSGNJ>
2022-03-02 17:58:47 +0530

    first commit

---
+++ DAY2/src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+		
+		int a = 2020;
+		int b = 1010;		
+		int c = a+b;
+		
+		System.out.println("Addition is "+c);
+	}
+
+}
